caa9c7c919df59f051d98966926cfdb842be327e
kelvin <kelvinguyen2@gmail.com>
2025-01-06 18:49:50 -0800

    Initial Commit

---
+++ plannr/plannrApp.swift
@@ -0,0 +1,17 @@
+//
+//  plannrApp.swift
+//  plannr
+//
+//  Created by Kelvin Nguyen on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct plannrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ plannrTests/plannrTests.swift
@@ -0,0 +1,17 @@
+//
+//  plannrTests.swift
+//  plannrTests
+//
+//  Created by Kelvin Nguyen on 1/6/25.
+//
+
+import Testing
+@testable import plannr
+
+struct plannrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
